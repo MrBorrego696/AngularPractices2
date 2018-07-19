a2182944f43a12abd6d1117d4435bff7f5bdb748
AlanIslas <alanislas696@gmail.com>
2018-07-19 16:02:24 -0500

    FastArrowFunctions
    Part2Arguments

---
+++ src/TypeScript/Ej04.js
@@ -0,0 +1,9 @@
+/////////////////////////////////////////////////////////////////////Fat Arrow Functions
+///////////////////////////////////Syntax
+setTimeout(function () {
+    console.log('setTimeout called!');
+}, 1000);
+//ES6
+setTimeout(function () {
+    console.log("setTimeout ES6 called!");
+}, 1000);
